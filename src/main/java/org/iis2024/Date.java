3fe6285efe4ef8577a4cb341118e08f565ed47c7
Ivan Gonzalez Dona <ivanglez06@gmail.com>
2025-03-10 22:18:47 +0100

    Added new variable variableIGD to Data.java

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -14,8 +14,11 @@ import java.text.SimpleDateFormat;
  *
  * Instances of this class are immutable, meaning that once a Date object is created, the day,
  * month, and year values cannot be changed.
+ *
+ * Update: Added new variable "variableIGD" from collaborator
  */
 public class Date {
+  private int variableIGD;
   private final int day;
   private final int month;
   private final int year;
